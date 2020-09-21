1368934cb6df1ef77c0b7c00b82b55619ff3ea51
judith <judithdemets@gmail.com>
2020-09-21 12:39:36 +0200

    Masonry fix

--- js.js
+++ js.js
@@ -1,56 +1,49 @@
 $(window).on('load', function () {
 
 
-    // masonry.pkgd.js added as external resource
 
     var grid = document.querySelector('.grid');
 
     var msnry = new Masonry(grid, {
         columnWidth: 100,
-        itemSelector: '.grid-item'
+        itemSelector: '.grid-item',
     });
 
+    $('#collapse-articles').hide();
 
 
-
-
-    $('#nav3 .nav-item .nav-link').click(function (event) {
-        msnry.layout();
-        console.log($(this).attr("class"));
+    $('#navbarDropdownHouse').mouseenter(function () {
         $('#main').addClass('dark-background');
-        $('#nav3 .nav-item .nav-link').removeClass('active');
-        $(this).addClass('active');
 
-
-
-
-        if (!$('#navbarDropdown1').attr('class').includes("active")) {
-
-            $('#collapse-articles').removeClass('show');
-            $('#main').removeClass('dark-background');
-            console.log('dropdownNOTactive');
-        }
-
-        if ($('#collapse-articles').attr('class').includes('show')) {
-            $('#navbarDropdown1').removeClass('active');
-            $('#main').removeClass('dark-background');
-        }
+        $('#collapse-articles').show();
 
     });
 
-    $('#nav4 .nav-item').click(function (event) {
+    $('#collapse-articles').mouseleave(function (event) {
+
+        $('#main').removeClass('dark-background');
+        $('#collapse-articles').hide();
+    });
+
+
+    if ($('#collapse-articles').css('display') == "block") {
+        msnry.layout();
+    }
+
+    $('#nav4 .nav-item').mouseenter(function () {
 
 
         var product = $(this).data('product');
-        console.log(product);
 
         $('#product-details').children().hide();
         $('#nav4 .nav-item .nav-link').removeClass('active');
         $('#product-details #' + product).show();
         $('#nav4 .nav-item #' + product + "-link").addClass('active');
 
-
     });
+
+
+
 
 
 
